7939034b59802c49bc136e8a445c6f431e789d3d
Piyush Mahapatra <piyushmahapatra001@gmail.com>
2020-06-13 05:57:10 +0530

    Fixed some typos

--- bitcoinAlert.py
+++ bitcoinAlert.py
@@ -7,8 +7,15 @@ from datetime import datetime
 from requests import Request, Session
 from tqdm import tqdm
 
-#URLS ARE HIDDEN FOR PRIVACY 
 
+# COINMARKET-API-URL
+COINMARKET_URL = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+
+#IFTTT URL:
+WEBHOOKS_TELEGRAM_URL = "https://maker.ifttt.com/trigger/bitcoinTelegram1/with/key/g2fW8tTnrigBgQ67BktfNdINEslhjApQsujQIx-eHKF"
+WEBHOOKS_SMS_URL = "https://maker.ifttt.com/trigger/bitcoinSMS/with/key/g2fW8tTnrigBgQ67BktfNdINEslhjApQsujQIx-eHKF"
+WEBHOOKS_PUSH_URL = "https://maker.ifttt.com/trigger/bitcoinPush/with/key/g2fW8tTnrigBgQ67BktfNdINEslhjApQsujQIx-eHKF"
+WEBHOOKS_TWITTER_URL = "https://maker.ifttt.com/trigger/bitcoinTwitter/with/key/g2fW8tTnrigBgQ67BktfNdINEslhjApQsujQIx-eHKF"
 
 
 # PARAMETERS
---
+++ bitcoinlogs.py
@@ -0,0 +1,8 @@
+# COINMARKET-API-URL
+COINMARKET_URL = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+
+#IFTTT URL:
+WEBHOOKS_TELEGRAM_URL = "https://maker.ifttt.com/trigger/bitcoinTelegram1/with/key/g2fW8tTnrigBgQ67BktfNdINEslhjApQsujQIx-eHKF"
+WEBHOOKS_SMS_URL = "https://maker.ifttt.com/trigger/bitcoinSMS/with/key/g2fW8tTnrigBgQ67BktfNdINEslhjApQsujQIx-eHKF"
+WEBHOOKS_PUSH_URL = "https://maker.ifttt.com/trigger/bitcoinPush/with/key/g2fW8tTnrigBgQ67BktfNdINEslhjApQsujQIx-eHKF"
+WEBHOOKS_TWITTER_URL = "https://maker.ifttt.com/trigger/bitcoinTwitter/with/key/g2fW8tTnrigBgQ67BktfNdINEslhjApQsujQIx-eHKF"

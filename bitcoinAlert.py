5fc97e1b907e5b11bf939b7bea035dd7f00853f6
Piyush Mahapatra <piyushmahapatra001@gmail.com>
2020-06-15 05:31:26 +0530

    Made some changes to protect Privacy

--- bitcoinAlert.py
+++ bitcoinAlert.py
@@ -7,15 +7,7 @@ from datetime import datetime
 from requests import Request, Session
 from tqdm import tqdm
 
-# COINMARKET-API-URL
-COINMARKET_URL = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
-
-#IFTTT URL:
-WEBHOOKS_TELEGRAM_URL = "https://maker.ifttt.com/trigger/bitcoinTelegram1/with/key/g2fW8tTnrigBgQ67BktfNdINEslhjApQsujQIx-eHKF"
-WEBHOOKS_SMS_URL = "https://maker.ifttt.com/trigger/bitcoinSMS/with/key/g2fW8tTnrigBgQ67BktfNdINEslhjApQsujQIx-eHKF"
-WEBHOOKS_PUSH_URL = "https://maker.ifttt.com/trigger/bitcoinPush/with/key/g2fW8tTnrigBgQ67BktfNdINEslhjApQsujQIx-eHKF"
-WEBHOOKS_TWITTER_URL = "https://maker.ifttt.com/trigger/bitcoinTwitter/with/key/g2fW8tTnrigBgQ67BktfNdINEslhjApQsujQIx-eHKF"
-
+#URLS ARE HIDDEN FOR PRIVACY 
 
 
 
